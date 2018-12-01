e57c1155c1ccdc88d358bdb2efe896e6fdd06cd1
SaravananRath <saravananr95m16@gmail.com>
2018-12-01 20:46:58 +0530

    Query with param and Messages Query

--- index.js
+++ index.js
@@ -6,27 +6,53 @@ const crypto = require('crypto')
 
 const db = {
     users: [
-        {id:1,name:'Saravanan',email:'sar@gam.com',avatarUrl:'https://www.image1.com'},
-        {id:2,name:'Deepak',email:'max@gam.com',avatarUrl:'https://www.image2.com'}
+        { id: "1", name: 'Saravanan', email: 'sar@gam.com', avatarUrl: 'https://www.image1.com' },
+        { id: "2", name: 'Deepak', email: 'max@gam.com', avatarUrl: 'https://www.image2.com' }
+    ],
+    messages: [
+        { id: "1", userId: "1", body: "Hello", createdAt: Date.now() },
+        { id: "2", userId: "2", body: "Hi", createdAt: Date.now() },
+        { id: "3", userId: "1", body: "What up?", createdAt: Date.now() },
     ]
 }
+
+class User {
+    constructor(user) {
+        Object.assign(this, user)
+    }
+    messages() {
+        return db.messages.filter(msg => msg.userId === this.id)
+    }
+
+}
+
 const schema = buildSchema(`
     type Mutation{
         addUser(email:String!,name:String):User
     }
     type Query{
         users:[User!]!
+        user(id:ID!):User
+        messages:[Message!]!
     }
     type User{
         id:ID!
         name:String
         email:String!
         avatarUrl:String
+        messages:[Message!]!
+    }
+    type Message{
+        id:ID!
+        userId:String
+        body:String!
+        createdAt:String
     }
 `)
 const rootValue = {
-    users: () => db.users,
-    addUser: ({email,name}) => {
+    users: () => db.users.map(user=> new User(user)),
+    messages: () => db.messages,
+    addUser: ({ email, name }) => {
         const user = {
             id: crypto.randomBytes(10).toString('hex'),
             email,
@@ -34,13 +60,14 @@ const rootValue = {
         }
         db.users.push(user)
         return user
-    }
+    },
+    user: ({ id }) => db.users.find(user => user.id === id)
 }
 
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
     schema,
     rootValue,
-    graphiql:true
+    graphiql: true
 }))
 
-app.listen(3000,()=>console.log('Listening on port 3000'))
+app.listen(3000, () => console.log('Listening on port 3000'))
