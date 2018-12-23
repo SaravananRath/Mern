a9042e42c8bf664b2e210eebaaf7f8112f65b01f
SaravananRath <saravananr95m16@gmail.com>
2018-12-23 13:33:52 +0530

    Chat App Setup

---
+++ src/resolvers/user.js
@@ -0,0 +1,13 @@
+export default {
+  Query: {
+    users: (root, arg, context, info) => {
+
+    },
+    user: (root, arg, context, info) => {
+
+    }
+  },
+  Mutation: {
+    signUp: (root, arg, context, info) => { }
+  }
+}
