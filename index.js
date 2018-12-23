a0bdabef4b1c9ef2dff10e0e02fa1abdd0588c73
SaravananRath <saravananr95m16@gmail.com>
2018-12-23 11:12:26 +0530

    app first commit

--- index.js
+++ index.js
@@ -57,5 +57,5 @@ const resolvers = {
 	}
 }
 
-const server = new ApolloServer({typeDefs,mocks:true}) 
+const server = new ApolloServer({typeDefs,resolvers}) 
 server.listen().then(({url})=> console.log(url))
