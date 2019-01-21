9fa2d2a7bdce78428e128f5ee6cb7ecb6a83cf2f
SaravananRath <saravananr95m16@gmail.com>
2019-01-21 12:25:51 +0530

    Custom Unique validation for email and username

--- src/models/user.js
+++ src/models/user.js
@@ -5,11 +5,17 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     validate: {
-      validator: email => User.where({ email }).countDocuments() === 0,
-      message: ({ value }) => `${value} already exists`
+      validator: email => User.unique({ email }),
+      message: ({ value }) => `Email ${value} already exists` // To do security
     }
   },
-  username: String,
+  username: {
+    type: String,
+    validate: {
+      validator: username => User.unique({ username }),
+      message: ({ value }) => `Username ${value} already exists` // To do security
+    }
+  },
   name: String,
   password: String
 }, {
@@ -21,6 +27,10 @@ userSchema.pre('save', async function () {
     this.password = await hash(this.password, 10)
   }
 })
+
+userSchema.statics.unique = async function (options) {
+  return await this.where(options).countDocuments() === 0
+}
 
 const User = mongoose.model('User', userSchema)
 export default User
